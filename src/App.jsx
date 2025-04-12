1572c2d976b9bd7b5526d87a47a4e7addf7ff58c
Adam Bastin <abast21@wgu.edu>
2025-04-11 21:28:28 -0400

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
 import Sidebar from "./components/Sidebar";
-import Details from "./pages/Details.jsx";
+import Portfolio from "./pages/Portfolio.jsx";
 import "./App.css";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Contact from "./pages/Contact.jsx";
+import HomePage from "./pages/Home.jsx";
+import Blog from "./pages/Blog.jsx";
 
 function App() {
 
     return (
-        <div className={"flex"}>
-            <div className={"flex-none w-40 md:w-40"}>
-                <Sidebar/>
+        <BrowserRouter>
+            <div className={"flex"}>
+                <div className={"flex-none w-40 2xl:w-90 sidebar text-xl"}>
+                    <Sidebar />
+                </div>
+                <main className={
+                    "content " +
+                    "flex-1 " +
+                    "flex-col " +
+                    "md:flex-column " +
+                    "2xl:text-2xl"
+                }>
+                        <Routes>
+                            <Route index element={<HomePage />} />
+                            <Route path={"portfolio"} element={<Portfolio />} />
+                            <Route path={"Blog"} element={<Blog />} />
+                            <Route path={"contact"} element={<Contact />} />
+                        </Routes>
+                </main>
             </div>
-            <main className={"flex-2"}>
-                <Details/>
-            </main>
-        </div>
+        </BrowserRouter>
     )
 }
 
