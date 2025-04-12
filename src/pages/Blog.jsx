68fda97e0ff7a527235691fb0d817f30e3ffbedc
Adam Bastin <abast21@wgu.edu>
2025-04-11 21:28:38 -0400

    Added temp page

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,10 @@
+export default function Blog (){
+    return (
+        <>
+            <h1>Blog</h1>
+            <ul className={"mt-6"}>
+                Check back soon
+            </ul>
+        </>
+    )
+}
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+export default function Contact (){
+    return (
+        <>
+            <h1>Contact</h1>
+            <ul className={"mt-6"}>
+                Check back soon
+            </ul>
+        </>
+    )
+}
