f9412a52c2a513444050d680f895ef7efa1dfe10
Adam Bastin <abast21@wgu.edu>
2025-04-11 21:31:32 -0400

    Changed some template data

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,30 +1,34 @@
+import "./Portfolio.css"
 
-
- export default function Details(){
+ export default function Portfolio(){
     return (
         <>
-            <div className={"p-4 md:p-10"}>
-                <div className={"border-solid border-1 grid grid-cols-3"}>
-                    <div className={"col-span-1 pt-4 pl-10"}>
-                        {/*Adam Bastin*/}
-                        Website title
+            <h1>Portfolio</h1>
+            <div className={"md:p-10"}>
+                <div className={"border-solid border-1 grid grid-cols-3 bg-gray-800"}>
+                    <div className={"col-span-1 m-auto text-left ml-4 text-2xl font-semibold"}>
+                        Adam Bastin
                     </div>
-                    <div className={"col-span-2 text-right pt-4 pr-10"}>
-                        Link / Link / Link / Link
-                        {/*<a href="https://github.com/AdamBastin">Github</a> /*/}
-                        {/*<a href="https://www.linkedin.com/in/adam-bastin-swe/">LinkedIn</a> /*/}
-                        {/*<a href={"blog.html"}>Blog</a> /*/}
-                        {/*<a href={"projects.html"}>Projects</a> /*/}
-                        {/*<a href={"contact.html"}>Contact</a>*/}
+                    <div className={"col-span-2 pt-4 pr-10 flex justify-around overflow-hidden"}>
+                        {/*
+                        When clicking a link, have it popup asking
+                        if they would like to see source code on GitHub
+                        or writeup on this website
+                        */}
+                        <a className={"Portfolio-Link"} href="https://github.com/AdamBastin"><div>GitHub</div></a>
+                        <a className={"Portfolio-Link"} href="https://www.linkedin.com/in/adam-bastin-swe/">LinkedIn</a>
+                        <a className={"Portfolio-Link"} href={"blog.html"}>Blog</a>
+                        <a className={"Portfolio-Link"} href={"projects.html"}>Projects</a>
+                        <a className={"Portfolio-Link"} href={"contact.html"}>Contact</a>
                     </div>
                     <div className={"col-span-3 flex justify-center items-center text-4xl p-4 md:p-10"}>
-                        Frontend for site
+                        Full Stack Developer
                     </div>
                     <div className={"col-span-1 border-solid border-1 border-r-0 border-l-0"}>
                         <div className={"text-lg/8 font-bold text-center"}>Projects</div>
-                        <ul className={"list-none divide-solid divide-y"}>
-                            <li className={"pl-1"}>Project 1</li>
-                            <li className={"pl-1"}>Project 2</li>
+                        <ul className={"list-none divide-solid divide-y text-left"}>
+                            <li className={"pl-1"}>Bastin.Dev</li>
+                            <li className={"pl-1"}>Knox Device Map</li>
                             <li className={"pl-1"}>Project 3</li>
                         </ul>
                     </div>
@@ -39,7 +43,7 @@
                     <div className={"col-span-1 border-solid border-1"}>
                         <div className={"text-lg/8 font-bold text-center"}>Education</div>
                         <ul className={"list-none divide-solid divide-y"}>
-                            <li className={"pl-1"}>Item 1</li>
+                            <li>B.S. Computer Science<br/>Western Governors University</li>
                         </ul>
                     </div>
                     <div className={"col-span-3 text-center p-4"}>
